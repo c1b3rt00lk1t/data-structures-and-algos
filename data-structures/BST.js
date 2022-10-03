a08853af779330a972e7215c6c42aeb92b1f0392
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-03 23:19:54 +0200

    Insert method included in the class for BST

--- data-structures/BST.js
+++ data-structures/BST.js
@@ -10,4 +10,41 @@ class BinarySearchTree {
     constructor(){
         this.root = null;
     }
+
+    insert(value) {
+
+        const node = new Node(value);
+        
+        if (this.root === null){
+            this.root = node;
+            return this;
+        } else {
+            let prev = this.root;
+            while(true){
+
+                if( value > prev.value){
+                    if (prev.right){
+                        prev = prev.right;
+                    } else {
+                        prev.right = node;
+                        return this;
+                    }
+                    
+                } else {
+                    if (prev.left){
+                        prev = prev.left;
+                    } else {
+                        prev.left = node;
+                        return this;
+                    }
+                    
+                }
+                
+                
+                
+            }
+        }
+    }
 }
+
+const tree = new BinarySearchTree(10)
