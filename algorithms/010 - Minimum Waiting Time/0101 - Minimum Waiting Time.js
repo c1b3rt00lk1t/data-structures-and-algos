d974689b2f01db3b1c40ada75a31e55a836a0b23
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-10 10:19:29 +0200

    Minimum Waiting Time solution with reduce.

---
+++ algorithms/010 - Minimum Waiting Time/0101 - Minimum Waiting Time.js
@@ -0,0 +1,10 @@
+function minimumWaitingTime(queries) {
+    return queries.sort((a,b) => a - b).reduce((acc,b,i,arr) => {
+      if( i > 0 ){
+        acc.last = acc.last + arr[i - 1];
+        acc.total = acc.total + acc.last;
+      }
+      return acc;
+    },{total: 0, last: 0}).total
+  }
+  
