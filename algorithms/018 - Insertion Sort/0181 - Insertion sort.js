0d58c10be26c53ebfc4a9f088db5574be97701c7
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-10-19 10:50:36 +0200

    First imperative solution with function helpers.

---
+++ algorithms/018 - Insertion Sort/0181 - Insertion sort.js
@@ -0,0 +1,33 @@
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++){
+      if(array[i] < array[i - 1]){
+        let position = findPosition(array, i);
+        insertInPosition(array, i, position);
+      }
+    }
+    return array;
+  }
+  
+  function findPosition(array, itemIndex){
+    for(let j = 0; j < itemIndex; j++){
+      if(array[j] > array[itemIndex]){
+        return j;
+      }
+    }
+    return itemIndex;
+  }
+  
+  function insertInPosition(array, itemIndex , targetPosition){
+    let item = array[itemIndex];
+  
+    for(let j = itemIndex; j > targetPosition; j--){
+      array[j] = array[j - 1];
+    }
+  
+    array[targetPosition] = item;
+  }
+  
+  
+  // Do not edit the line below.
+  exports.insertionSort = insertionSort;
+  
