e8589a255faa409143c62ef41fe966071d975a59
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-08-03 16:20:44 +0200

    Unit tests for iterative solution Breadth-first Search.

---
+++ algorithms/067 - Breadth-first Search/067 - Breadth-first Search.test.js
@@ -0,0 +1,51 @@
+const Node = require('./067 - Breadth-first Search')
+
+describe('Test the Breadth-first Search algoritm', () =>{
+
+    test('returns abcd', () =>{
+        const graph = new Node('A');
+        graph.addChild('B')
+        graph.addChild('C')
+        graph.addChild('D')
+
+        const result = graph.breadthFirstSearch([]);
+        const expected = ['A','B','C','D'];
+        expect(result).toStrictEqual(expected);
+
+    })
+
+    test('returns abcdefgh', () =>{
+        const graph = new Node('A');
+        graph.addChild('B')
+        graph.addChild('C')
+        graph.addChild('D')
+        graph.children[0].addChild('E')
+        graph.children[0].addChild('F')
+        graph.children[2].addChild('G')
+        graph.children[2].addChild('H')
+        const result = graph.breadthFirstSearch([]);
+        const expected = ['A','B','C','D','E','F','G','H'];
+        expect(result).toStrictEqual(expected)
+
+    })
+
+
+    test('returns abcdefghijk', () =>{
+        const graph = new Node('A');
+        graph.addChild('B')
+        graph.addChild('C')
+        graph.addChild('D')
+        graph.children[0].addChild('E')
+        graph.children[0].addChild('F')
+        graph.children[2].addChild('G')
+        graph.children[2].addChild('H')
+        graph.children[0].children[1].addChild('I')
+        graph.children[0].children[1].addChild('J')
+        graph.children[2].children[0].addChild('K')
+
+        const result = graph.breadthFirstSearch([]);
+        const expected = ['A','B','C','D','E','F','G','H','I','J','K'];
+        expect(result).toStrictEqual(expected)
+
+    })
+});
