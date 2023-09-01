f2e6aaf2154f5ad6da7ea1c070c24a65216df0dd
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-09-01 22:00:39 +0200

    Added parameter in the function call

--- algorithms/015b - Optimal Freelancing/015b1  -Optimal Freelancing.test.js
+++ algorithms/015b - Optimal Freelancing/015b1  -Optimal Freelancing.test.js
@@ -1,6 +1,5 @@
 const optimalFreelancing = require("./015b1 - Optimal Freelancing");
 
-optimalFreelancing();
 describe("Test suite for Optimal Freelancing", () => {
   test("initial example", () => {
     const jobs = [
@@ -8,7 +7,7 @@ describe("Test suite for Optimal Freelancing", () => {
       { deadline: 2, payment: 1 },
       { deadline: 2, payment: 2 },
     ];
-    const result = optimalFreelancing();
+    const result = optimalFreelancing(jobs);
     expect(result).toBe(3);
   });
 });
