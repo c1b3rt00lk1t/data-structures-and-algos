2535c90c13b15181c4403352bd199ddf16f95523
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-09-01 23:54:17 +0200

    Added falining test (gaps in deadline)

--- algorithms/015b - Optimal Freelancing/015b1  -Optimal Freelancing.test.js
+++ algorithms/015b - Optimal Freelancing/015b1  -Optimal Freelancing.test.js
@@ -10,4 +10,42 @@ describe("Test suite for Optimal Freelancing", () => {
     const result = optimalFreelancing(jobs);
     expect(result).toBe(3);
   });
+
+  test("test case 9", () => {
+    //  [{"deadline": 2, "payment": 7}, {"deadline": 2, "payment": 6}, {"deadline": 2, "payment": 5}, {"deadline": 2, "payment": 4}, {"deadline": 2, "payment": 3}, {"deadline": 2, "payment": 2}, {"deadline": 2, "payment": 1}]
+    {
+      const jobs = [
+        {
+          deadline: 2,
+          payment: 1,
+        },
+        {
+          deadline: 2,
+          payment: 2,
+        },
+        {
+          deadline: 2,
+          payment: 3,
+        },
+        {
+          deadline: 2,
+          payment: 4,
+        },
+        {
+          deadline: 2,
+          payment: 5,
+        },
+        {
+          deadline: 2,
+          payment: 6,
+        },
+        {
+          deadline: 2,
+          payment: 7,
+        },
+      ];
+      const result = optimalFreelancing(jobs);
+      expect(result).toBe(13);
+    }
+  });
 });
