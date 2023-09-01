6494daa7f39cb034a403188b4fd250b56e5f6408
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-09-01 22:03:05 +0200

    Code passing 10 from 13 test cases in the web

--- algorithms/015b - Optimal Freelancing/015b1 - Optimal Freelancing.js
+++ algorithms/015b - Optimal Freelancing/015b1 - Optimal Freelancing.js
@@ -1,5 +1,36 @@
-function optimalFreelancing() {
-  return 3;
+function optimalFreelancing(jobs) {
+  const optimalPayment = jobs
+    .sort(orderCriteria)
+    .reduce((acc, curr, i, arr) => {
+      if (i >= 1 && arr[i].deadline != arr[i - 1].deadline) {
+        acc += arr[i].payment;
+      } else if (i === 0) {
+        acc += arr[i].payment;
+      }
+      return acc;
+    }, 0);
+
+  return optimalPayment;
+}
+
+function orderCriteria(a, b) {
+  const { deadline: deadline1, payment: payment1 } = a;
+  const { deadline: deadline2, payment: payment2 } = b;
+
+  const order = -1;
+  let orderResult;
+
+  if (+deadline1 > +deadline2) {
+    orderResult = -order;
+  } else if (+deadline1 < +deadline2) {
+    orderResult = order;
+  } else if (+payment1 > +payment2) {
+    orderResult = order;
+  } else if (+payment1 < +payment2) {
+    orderResult = -order;
+  }
+  console.log(a, b, orderResult);
+  return orderResult;
 }
 
 module.exports = optimalFreelancing;
