939ca3f09fb3da7438373eb77fd9268069867f33
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-09-02 00:36:15 +0200

    Tests if the optimal is not sequential

--- algorithms/015b - Optimal Freelancing/015b1  -Optimal Freelancing.test.js
+++ algorithms/015b - Optimal Freelancing/015b1  -Optimal Freelancing.test.js
@@ -11,7 +11,7 @@ describe("Test suite for Optimal Freelancing", () => {
     expect(result).toBe(3);
   });
 
-  test("test case 9", () => {
+  test("gaps in deadline", () => {
     //  [{"deadline": 2, "payment": 7}, {"deadline": 2, "payment": 6}, {"deadline": 2, "payment": 5}, {"deadline": 2, "payment": 4}, {"deadline": 2, "payment": 3}, {"deadline": 2, "payment": 2}, {"deadline": 2, "payment": 1}]
     {
       const jobs = [
@@ -47,5 +47,48 @@ describe("Test suite for Optimal Freelancing", () => {
       const result = optimalFreelancing(jobs);
       expect(result).toBe(13);
     }
+  });
+  test("test case 11", () => {
+    // [{"deadline": 1, "payment": 4}, {"deadline": 1, "payment": 3}, {"deadline": 2, "payment": 1}, {"deadline": 3, "payment": 2}, {"deadline": 4, "payment": 3}, {"deadline": 4, "payment": 2}, {"deadline": 4, "payment": 1}, {"deadline": 5, "payment": 4}, {"deadline": 8, "payment": 1}]
+    const jobs = [
+      {
+        deadline: 2,
+        payment: 1,
+      },
+      {
+        deadline: 1,
+        payment: 4,
+      },
+      {
+        deadline: 3,
+        payment: 2,
+      },
+      {
+        deadline: 1,
+        payment: 3,
+      },
+      {
+        deadline: 4,
+        payment: 3,
+      },
+      {
+        deadline: 4,
+        payment: 2,
+      },
+      {
+        deadline: 4,
+        payment: 1,
+      },
+      {
+        deadline: 5,
+        payment: 4,
+      },
+      {
+        deadline: 8,
+        payment: 1,
+      },
+    ];
+    const result = optimalFreelancing(jobs);
+    expect(result).toBe(16);
   });
 });
