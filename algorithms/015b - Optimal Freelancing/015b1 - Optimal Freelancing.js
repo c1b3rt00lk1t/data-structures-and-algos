05b5bacf30045de5dbc6a4ec37377c6698be5ee8
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-09-02 09:05:16 +0200

    Code that pass that optimal is not sequential

--- algorithms/015b - Optimal Freelancing/015b1 - Optimal Freelancing.js
+++ algorithms/015b - Optimal Freelancing/015b1 - Optimal Freelancing.js
@@ -1,44 +1,37 @@
 function optimalFreelancing(jobs) {
-  const optimalPayment = jobs.sort(orderCriteria).reduce(
-    (acc, curr, i, arr) => {
-      if (i >= 1 && arr[i].deadline != arr[i - 1].deadline) {
-        acc.result += arr[i].payment;
-        acc.lastDeadline++;
-      } else if (
-        i >= 1 &&
-        arr[i].deadline == arr[i - 1].deadline &&
-        acc.possible
-      ) {
-        acc.result += arr[i].payment;
-        acc.possible--;
-      } else if (i === 0) {
-        acc.result += arr[i].payment;
-        acc.possible = arr[i].deadline - 1;
-      }
-      return acc;
-    },
-    { result: 0, lastDeadline: 0, possible: 0 }
-  );
+  const LENGTH_OF_WEEK = 7;
+  let profit = 0;
+  const timeline = Array.from({ length: LENGTH_OF_WEEK }, (_) => false);
+  jobs.sort(orderCriteria);
 
-  return optimalPayment.result;
+  for (const job of jobs) {
+    const maxTime = Math.min(job.deadline, LENGTH_OF_WEEK);
+    for (let i = maxTime - 1; i >= 0; i--) {
+      if (timeline[i] === false) {
+        profit += job.payment;
+        timeline[i] = true;
+        break;
+      }
+    }
+  }
+  return profit;
+
+  // return jobs.sort(orderCriteria);
 }
 
 function orderCriteria(a, b) {
-  const { deadline: deadline1, payment: payment1 } = a;
-  const { deadline: deadline2, payment: payment2 } = b;
+  const { payment: payment1 } = a;
+  const { payment: payment2 } = b;
 
   const order = -1;
   let orderResult;
 
-  if (+deadline1 > +deadline2) {
-    orderResult = -order;
-  } else if (+deadline1 < +deadline2) {
+  if (+payment1 > +payment2) {
     orderResult = order;
-  } else if (+payment1 > +payment2) {
-    orderResult = order;
-  } else if (+payment1 < +payment2) {
+  } else if (+payment1 <= +payment2) {
     orderResult = -order;
   }
+
   console.log(a, b, orderResult);
   return orderResult;
 }
