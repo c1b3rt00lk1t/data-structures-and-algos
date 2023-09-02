a690475425b4518d2e3b9fcc12c9963c61a1e9f5
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-09-02 09:17:42 +0200

    all payments equal

--- algorithms/015b - Optimal Freelancing/015b1  -Optimal Freelancing.test.js
+++ algorithms/015b - Optimal Freelancing/015b1  -Optimal Freelancing.test.js
@@ -92,7 +92,8 @@ describe("Test suite for Optimal Freelancing", () => {
     expect(result).toBe(16);
   });
 
-  test("test 13", () => {
+  test("all payments equal", () => {
+    // [{"deadline": 1, "payment": 1}, {"deadline": 2, "payment": 1}, {"deadline": 3, "payment": 1}, {"deadline": 4, "payment": 1}, {"deadline": 5, "payment": 1}, {"deadline": 6, "payment": 1}, {"deadline": 7, "payment": 1}, {"deadline": 8, "payment": 1}, {"deadline": 9, "payment": 1}, {"deadline": 10, "payment": 1}]
     const jobs = [
       {
         deadline: 1,
