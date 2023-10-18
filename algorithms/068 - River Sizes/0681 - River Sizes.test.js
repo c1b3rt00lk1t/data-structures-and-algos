28a5a9178e53ece9608ef7818a35d3e24cce8b85
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-10-18 09:15:40 +0200

    Test simple matrix with adjacent river cells

---
+++ algorithms/068 - River Sizes/0681 - River Sizes.test.js
@@ -0,0 +1,13 @@
+const riverSizes = require("./0681 - River Sizes");
+
+describe("Test suite for river sizes", () => {
+  it("Test simple matrix with adjacent river cells", () => {
+    const matrix = [
+      [1, 0],
+      [1, 0],
+    ];
+    const expected = [2];
+    const result = riverSizes(matrix);
+    expect(result).toStrictEqual(expected);
+  });
+});
