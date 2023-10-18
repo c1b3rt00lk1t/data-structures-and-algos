b749c7091426d24c753c1d6ac790ec959ed858cc
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-10-18 09:35:00 +0200

    Test River Sizes ts solution

---
+++ algorithms/068 - River Sizes/0682 - River Sizes.test.ts
@@ -0,0 +1,33 @@
+import { riverSizes } from "./0682 - River Sizes";
+
+describe("Test suite for river sizes", () => {
+  it("Test simple matrix with adjacent river cells", () => {
+    const matrix = [
+      [1, 0],
+      [1, 0],
+    ];
+    const expected = [2];
+    const result = riverSizes(matrix);
+    expect(result).toStrictEqual(expected);
+  });
+
+  it("Test simple matrix with no adjacent river cells", () => {
+    const matrix = [
+      [1, 0],
+      [0, 1],
+    ];
+    const expected = [1, 1];
+    const result = riverSizes(matrix);
+    expect(result).toStrictEqual(expected);
+  });
+
+  it("Test matrix with irregular river cells", () => {
+    const matrix = [
+      [1, 0, 1],
+      [0, 1, 1],
+    ];
+    const expected = [1, 3];
+    const result = riverSizes(matrix);
+    expect(result).toStrictEqual(expected);
+  });
+});
