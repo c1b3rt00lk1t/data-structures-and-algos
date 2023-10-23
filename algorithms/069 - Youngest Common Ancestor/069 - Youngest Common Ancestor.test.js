05acb483d57eae8fba03b85544669a9bf80cb313
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-10-23 19:28:42 +0200

    Top ancestor is the youngest common ancestor

--- algorithms/069 - Youngest Common Ancestor/069 - Youngest Common Ancestor.test.js
+++ algorithms/069 - Youngest Common Ancestor/069 - Youngest Common Ancestor.test.js
@@ -1,4 +1,26 @@
-const AncestralTree = require("./069 - Youngest Common Ancestor");
-const getYoungestCommonAncestor = require("./069 - Youngest Common Ancestor");
+const {
+  AncestralTree,
+  getYoungestCommonAncestor,
+} = require("./069 - Youngest Common Ancestor");
 
-describe("Tests for Youngest Common Ancestor", () => {});
+require("./069 - Youngest Common Ancestor").default;
+
+describe("Tests for Youngest Common Ancestor", () => {
+  test("Top ancestor is the youngest common ancestor", () => {
+    const topAncestor = new AncestralTree("A");
+    const descendantOne = new AncestralTree("B");
+    descendantOne.ancestor = topAncestor;
+    const descendantTwo = new AncestralTree("C");
+    descendantTwo.ancestor = topAncestor;
+
+    const expected = "A";
+
+    const result = getYoungestCommonAncestor(
+      topAncestor,
+      descendantOne,
+      descendantTwo
+    );
+    console.log(result);
+    expect(result?.name).toStrictEqual(expected);
+  });
+});
