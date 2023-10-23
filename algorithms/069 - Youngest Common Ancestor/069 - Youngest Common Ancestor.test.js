ae33c22f589ecf05e091e5a33ffc938750bd09fd
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-10-23 18:57:44 +0200

    Basic describe testing setup

--- algorithms/069 - Youngest Common Ancestor/069 - Youngest Common Ancestor.test.js
+++ algorithms/069 - Youngest Common Ancestor/069 - Youngest Common Ancestor.test.js
@@ -1,2 +1,4 @@
 const AncestralTree = require("./069 - Youngest Common Ancestor");
 const getYoungestCommonAncestor = require("./069 - Youngest Common Ancestor");
+
+describe("Tests for Youngest Common Ancestor", () => {});
