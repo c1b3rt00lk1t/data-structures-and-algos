0f5212d47efbac4a90ae4669f3c3b61125e0f221
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-10-23 18:56:15 +0200

    Basic class and empty function

---
+++ algorithms/069 - Youngest Common Ancestor/069 - Youngest Common Ancestor.js
@@ -0,0 +1,11 @@
+class AncestralTree {
+  constructor(name) {
+    this.name = name;
+    this.ancestor = null;
+  }
+}
+
+function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {}
+
+module.exports = AncestralTree;
+module.exports = getYoungestCommonAncestor;
