d629dc51c2f6b8989108ac350037d7c653026d05
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-10-23 19:56:42 +0200

    JS non optimal solution

--- algorithms/069 - Youngest Common Ancestor/069 - Youngest Common Ancestor.js
+++ algorithms/069 - Youngest Common Ancestor/069 - Youngest Common Ancestor.js
@@ -5,7 +5,30 @@ class AncestralTree {
   }
 }
 
-function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {}
+function compareByName(tree1, tree2) {
+  return tree1.name === tree2.name;
+}
 
-module.exports = AncestralTree;
-module.exports = getYoungestCommonAncestor;
+function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
+  const ancestorListOne = [];
+  let ancestorOne = descendantOne;
+  while (ancestorOne) {
+    ancestorListOne.push(ancestorOne);
+    ancestorOne = ancestorOne.ancestor;
+  }
+
+  const ancestorListTwo = [];
+  let ancestorTwo = descendantTwo;
+  while (ancestorTwo) {
+    ancestorListTwo.push(ancestorTwo);
+    ancestorTwo = ancestorTwo.ancestor;
+  }
+
+  for (let anc1 of ancestorListOne) {
+    if (ancestorListTwo.find((anc2) => anc2.name === anc1.name)) {
+      return anc1;
+    }
+  }
+}
+
+module.exports = { AncestralTree, getYoungestCommonAncestor };
